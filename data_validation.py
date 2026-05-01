16a1ef1eced9ecd246b59348bb078cb03fd09dae
Jun Aishima <jaishima@bnl.gov>
2026-05-01 13:49:54 -0400

    update data_validation to use dotenv, separate getting data

--- data_validation.py
+++ data_validation.py
@@ -1,28 +1,52 @@
+import os
 from prefect import task, flow, get_run_logger
-from prefect.blocks.system import Secret
 import time as ttime
-from tiled.client import from_profile
+from tiled.client import from_uri
+from dotenv import load_dotenv
+
+
+def get_api_key_from_env(api_key=None):
+    with open("/srv/container.secret", "r") as secrets:
+        load_dotenv(stream=secrets)
+    api_key = os.environ["TILED_API_KEY"]
+    return api_key
 
 
 @task(retries=2, retry_delay_seconds=10)
-def read_all_streams(uid):
-    api_key = Secret.load("tiled-chx-api-key", _sync=True).get()
-    tiled_client = from_profile("nsls2", api_key=api_key)
+def get_run(uid, api_key=None):
+    if not api_key:
+        api_key = get_api_key_from_env()
+    cl = from_uri("https://tiled.nsls2.bnl.gov", api_key=api_key)
+    run = cl["chx/raw"][uid]
+    return run
+
+
+@task(retries=2, retry_delay_seconds=10)
+def get_run_sandbox(uid, api_key=None):
+    if not api_key:
+        api_key = get_api_key_from_env()
+    cl = from_uri("https://tiled.nsls2.bnl.gov", api_key=api_key)
+    run = cl["chx/sandbox"][uid]
+    return run
+
+
+@task(retries=2, retry_delay_seconds=10)
+def read_stream(run, stream):
+    return run[stream].read()
+
+
+@flow
+def data_validation(uid, api_key=None):
     logger = get_run_logger()
-    run = tiled_client['chx']["raw"][uid]
+    run = get_run(uid, api_key=api_key)
     logger.info(f"Validating uid {run.start['uid']}")
     start_time = ttime.monotonic()
     for stream in run:
         logger.info(f"{stream}:")
         stream_start_time = ttime.monotonic()
-        stream_data = run[stream].read()
+        stream_data = read_stream(run, stream)
         stream_elapsed_time = ttime.monotonic() - stream_start_time
         logger.info(f"{stream} elapsed_time = {stream_elapsed_time}")
         logger.info(f"{stream} nbytes = {stream_data.nbytes:_}")
     elapsed_time = ttime.monotonic() - start_time
     logger.info(f"{elapsed_time = }")
-
-
-@flow
-def data_validation(uid):
-    read_all_streams(uid)
